7069053d3133b6dc77b175cfe317d471e8f01cfa
Pakobbix <Pakobbix@gmail.com>
2023-05-22 21:05:46 +0200

    removed the default load page, due to errors with the surfbar.

--- restart.sh
+++ restart.sh
@@ -13,7 +13,7 @@ if [ -f /tmp/ebesucher_restart.lock ]; then
 fi
 
 # Erstelle Lockfile
-echo $$ > /tmp/ebesucher_restart.lock
+echo $$ >/tmp/ebesucher_restart.lock
 
 # Ordner des Skripts
 git_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
@@ -31,22 +31,16 @@ unzip -qqo config.zip
 # Warte 60s um die 120s Wartezeit von Ebesucher zu umgehen
 sleep 60s
 # Starte Ebesucher Firefox
-docker_run(){
-  if [ -z "$username" ]; then
-    # Funktion zum starten des Containers, falls username nicht gesetzt
-    docker run -d --name="$containername" -p 3000:5800 -v "$git_dir"/config:/config:rw -m "$1" --cpus "$CPUcores" --shm-size 2g jlesage/firefox
-    else
-    # Funktion zum starten von Firefox immer mit der Surfbar (Verhindert dass bei Updates Firefox ein "neue Features" Tab über der Surfbar öffnet)
-    docker run -d --name="$containername" -p 3000:5800 -e FF_OPEN_URL="https://ebesucher.com/surfbar/$username" -m "$1" --cpus "$CPUcores" -v "$git_dir"/config:/config:rw --shm-size 2g jlesage/firefox
-  fi
+docker_run() {
+  docker run -d --name="$containername" -p 3000:5800 -v "$git_dir"/config:/config:rw -m "$1" --cpus "$CPUcores" --shm-size 2g jlesage/firefox
 }
 
 # Überprüfe wieviel Arbeitsspeicher vorhanden ist, um bei Geräten mit weniger als 4 GB 1 GB als Sicherungs zu haben.
 ram_avail=$(bc <<<"scale=2;$(($(free | awk 'NR==2 {print $2}') / 1000000))")
 if ((ram_avail >= 3 && ram_avail <= 4)); then
-	docker_run "3g" "$CPUSet"
+  docker_run "3g" "$CPUSet"
 elif [[ $ram_avail -ge "5" ]]; then
-	docker_run "4g" "$CPUSet"
+  docker_run "4g" "$CPUSet"
 fi
 
 rm -f /tmp/ebesucher_restart.lock
